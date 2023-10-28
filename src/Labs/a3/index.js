a660d5bf214662f20ae8e90c6556820f1ce69021
Wanhao Zhou <wanhao.zhou@gmail.com>
2023-10-28 13:53:24 -0400

    a4: labs

--- src/Labs/a3/index.js
+++ src/Labs/a3/index.js
@@ -4,10 +4,21 @@ import Classes from "./Classes";
 import Styles from './Styles';
 import ConditionalOutput from "./ConditionalOutput";
 import TodoList from "./todos/TodoList";
+import { useSelector } from "react-redux";
+
 
 const Assignment3 = () => {
+    const { todos } = useSelector((state) => state.todosReducer);
+
     return (
         <div className="container">
+            <ul className="list-group">
+                {todos.map((todo) => (
+                    <li className="list-group-item" key={todo.id}>
+                        {todo.title}
+                    </li>
+                ))}
+            </ul>
             <h1>Assignment 3</h1>
             <TodoList />
             <ConditionalOutput />
---
+++ src/Labs/a4/ReduxExamples/todos/TodoList.js
@@ -0,0 +1,24 @@
+import { useSelector } from "react-redux";
+
+import TodoForm from "./TodoForm";
+import TodoItem from "./TodoItem";
+
+function TodoList() {
+    const { todos } = useSelector((state) => state.todosReducer);
+
+    return (
+        <div>
+            <h2>Todo List</h2>
+            <ul className="list-group">
+                <TodoForm />
+                {todos.map((todo) => (
+                    <TodoItem
+                        todo={todo} />
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default TodoList;
+
