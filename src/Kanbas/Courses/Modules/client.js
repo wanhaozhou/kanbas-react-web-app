d982885b0125558298c1703c87f3fd03fce2865d
Wanhao Zhou <wanhao.zhou@gmail.com>
2023-11-09 16:03:17 -0500

    a5: minor

--- src/Kanbas/Courses/Modules/client.js
+++ src/Kanbas/Courses/Modules/client.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const API_BASE = process.env.REACT_APP_API_BASE;
+export const API_BASE = process.env.REACT_APP_API_BASE || 'http://localhost:4000/api';
 export const COURSES_URL = `${API_BASE}/courses`;
 export const MODULES_URL = `${API_BASE}/modules`;
 
