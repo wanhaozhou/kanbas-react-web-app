a4797b37d2885f3b7be7e8d84f218ab336e64124
Wanhao Zhou <wanhao.zhou@gmail.com>
2023-11-09 13:36:03 -0500

    a5: labs

--- src/Labs/a5/WorkingWithArrays.js
+++ src/Labs/a5/WorkingWithArrays.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 const WorkingWithArrays = () => {
+    const API = "http://localhost:4000/a5/todos";
+    const [errorMessage, setErrorMessage] = useState(null);
 
     const [todo, setTodo] = useState({
         id: 1,
@@ -38,13 +40,53 @@ const WorkingWithArrays = () => {
         setTodos(response.data);
     };
 
+    const postTodo = async () => {
+        const response = await axios.post(API, todo);
+        setTodos([...todos, response.data]);
+    };
+
+    const deleteTodo = async (todo) => {
+        try {
+            const response = await axios.delete(
+                `${API}/${todo.id}`);
+            setTodos(todos.filter((t) => t.id !== todo.id));
+        } catch (error) {
+            console.log(error);
+            setErrorMessage(error.response.data.message);
+        }
+    };
+
+    const updateTodo = async () => {
+        // const response = await axios.put(
+        //     `${API}/${todo.id}`, todo);
+        // setTodos(todos.map((t) => (
+        //     t.id === todo.id ? todo : t)));
+        // // setTodo({});
+        try {
+            const response = await axios.put(
+                `${API}/${todo.id}`, todo);
+            setTodos(todos.map((t) => (
+                t.id === todo.id ? todo : t)));
+            // setTodo({});
+        } catch (error) {
+            console.log(error);
+            setErrorMessage(error.response.data.message);
+        }
+
+    };
 
 
-    const API = "http://localhost:4000/a5/todos";
     return (
         <div>
             <h3>Working with Arrays</h3>
-
+            <button onClick={createTodo}
+                className="btn btn-primary mb-2 w-100">
+                Create Todo
+            </button>
+            <button onClick={updateTitle}
+                className="btn btn-success mb-2 w-100">
+                Update Title
+            </button>
             <input
                 value={todo.id}
                 onChange={(e) => setTodo({
@@ -61,19 +103,61 @@ const WorkingWithArrays = () => {
                 className="form-control mb-2"
                 type="text"
             />
-            <button onClick={createTodo}
-                className="btn btn-primary mb-2 w-100">
-                Create Todo
+            <textarea
+                className="form-control"
+                onChange={(e) => setTodo({
+                    ...todo,
+                    description: e.target.value
+                })}
+                value={todo.description || ''} type="text"
+            />
+            <input
+                className="form-control"
+                onChange={(e) => setTodo({
+                    ...todo, due: e.target.value
+                })}
+                value={todo.due || ''} type="date"
+            />
+            <label>
+                <input
+                    onChange={(e) => setTodo({
+                        ...todo, completed: e.target.checked
+                    })}
+                    checked={todo.completed} type="checkbox"
+                />
+                Completed
+            </label>
+            <br />
+            <button
+                className="btn btn-primary"
+                onClick={postTodo} >
+                Post Todo
             </button>
-            <button onClick={updateTitle}
-                className="btn btn-success mb-2 w-100">
-                Update Title
+            <button className="btn btn-primary" onClick={updateTodo}>
+                Update Todo
             </button>
+            <button className="btn btn-danger" onClick={() => { deleteTodo(todo); deleteTodo(todo); }}>
+                3.5.4 Delete Todo Twice (shows an error message)
+            </button>
+            {errorMessage && (
+                <div className="alert alert-danger mb-2 mt-2">
+                    {errorMessage}
+                </div>
+            )}
+
             <ul className="list-group">
                 {todos.map((todo) => (
                     <li key={todo.id}
                         className="list-group-item">
                         {todo.title}
+                        <p>
+                            <input
+                                checked={todo.completed}
+                                type="checkbox" readOnly
+                            />
+                        </p>
+                        <p>{todo.description || ''}</p>
+                        <p>{todo.due || ''}</p>
                         <button
                             onClick={() => fetchTodoById(todo.id)}
                             className="btn btn-warning me-2 float-end" >
@@ -82,7 +166,12 @@ const WorkingWithArrays = () => {
                         <button
                             onClick={() => removeTodo(todo)}
                             className="btn btn-danger float-end" >
-                            Remove
+                            Remove (HTTP Get)
+                        </button>
+                        <button
+                            onClick={() => deleteTodo(todo)}
+                            className="btn btn-danger float-end ms-2">
+                            Delete (HTTP Delete)
                         </button>
                     </li>
                 ))}
@@ -170,7 +259,7 @@ const WorkingWithArrays = () => {
 
             <h3>Updating description</h3>
             <input
-                value={todo.description}
+                value={todo.description || ''}
                 onChange={(e) => setTodo({
                     ...todo, description: e.target.value
                 })}
@@ -180,7 +269,7 @@ const WorkingWithArrays = () => {
             <a
                 href={`${API}/${todo.id}/description/${todo.description}`}
                 className="btn btn-primary me-2" >
-                Update description to {todo.description}
+                Update description to {todo.description || ''}
             </a>
 
         </div>
